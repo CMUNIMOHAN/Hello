631cd29504e9eed07df30a7cbdd11c4a331fc353
cmuniomkar9@gmail.com <CMUNI OMKAR@CMUNIOMKAR-PC>
2017-04-11 18:03:21 +0530

    second change

--- FirstJAVA/src/HelloDemo.java
+++ FirstJAVA/src/HelloDemo.java
@@ -4,6 +4,6 @@ public class HelloDemo {
 	public static void main(String[] args) {
 		System.out.println("Hello java");
 		System.out.println("Welcome to javaCampus");
+		System.out.println("MuniMohan");
 	}
-
 }
